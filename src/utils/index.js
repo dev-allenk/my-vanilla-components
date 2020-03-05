39ec8dd949830a56dd2c9a4ac150915a0e70d706
Heungsoo, Kim <dev.allenk@gmail.com>
2020-03-06 01:58:41 +0900

    Revert "[1] 간단한 할일 메모 앱 작성 (#1)"
    This reverts commit 5732bca073554bf9b414e33e41b39fa6cc28eaa3.

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export const delegate = ({ element, eventType, className, callback }) => {
-  element.addEventListener(eventType, e => {
-    e.target.classList.contains(className) && callback(e);
-  });
-};
